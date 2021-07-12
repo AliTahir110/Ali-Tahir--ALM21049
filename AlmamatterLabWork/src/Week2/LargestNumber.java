c5ff85134826af59cba9808346027fc6fa06c485
HP <HP@DESKTOP-P198CBN>
2021-07-13 00:48:34 +0530

    1

--- AlmamatterLabWork/src/Week2/LargestNumber.java
+++ AlmamatterLabWork/src/Week2/LargestNumber.java
@@ -2,6 +2,13 @@ package Week2;
 
 public class LargestNumber {
 public static void main(String[] args) {
-	
+	double n1=-5.5, n2= 4.5,n3=3.5;
+	if (n1>=n2 && n1>=n3)
+		System.out.println(n1+ "n1 is the largest number");
+	else if(n2>=n1 && n2>=n3)
+	System.out.println(n2+  "Largest number");
+	//can use this tooelse if (n3>=n1 && n3>=n2)
+	else
+	System.out.println(n3+  "Largest Number");
 }
 }
